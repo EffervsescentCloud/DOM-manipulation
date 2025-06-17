fe1425b033244ed210ce0ff210f17b206fc44d21
GEOFFREY Fisher Seufert <7003549@cpsd.us>
2025-06-17 12:09:13 -0400

    did the other assignment

--- A dynamic shopping list/index.js
+++ A dynamic shopping list/index.js
@@ -5,16 +5,20 @@ const button = document.querySelector("button")
 function addItem() {
     let item = input.value
     input.value = ''
-    console.log(item + " is the item")
     let newLi = document.createElement("li")
     let newSpan = document.createElement("span")
     let newButton = document.createElement("button")
     newSpan.textContent = item
     newButton.textContent = "delete"
+    newButton.addEventListener("click", () => {
+        newLi.remove()
+        newSpan.remove()
+        newButton.remove()
+    })
     newLi.appendChild(newSpan)
     newLi.appendChild(newButton)
     list.appendChild(newLi)
-    
+    newLi.focus()
 }
 
 button.addEventListener("click", addItem)
